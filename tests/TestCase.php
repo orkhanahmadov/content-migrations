05bb961707a45fb41959763433c7e1fcc5c88a9f
Orkhan Ahmadov <ahmadov90@gmail.com>
2020-09-25 09:54:22 +0200

    TestCase tearDown fix

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -10,13 +10,15 @@ class TestCase extends \Orchestra\Testbench\TestCase
 {
     protected function tearDown(): void
     {
-        Collection::make(scandir(database_path() . '/content-migrations/'))
-            ->filter(function ($file) {
-                return Str::endsWith($file, '.php');
-            })
-            ->each(function ($file) {
-                unlink(database_path() . '/content-migrations/' . $file);
-            });
+        if (file_exists(database_path() . DIRECTORY_SEPARATOR . 'content-migrations')) {
+            Collection::make(scandir(database_path() . DIRECTORY_SEPARATOR . 'content-migrations'))
+                ->filter(function ($file) {
+                    return Str::endsWith($file, '.php');
+                })
+                ->each(function ($file) {
+                    unlink(database_path() . '/content-migrations/' . $file);
+                });
+        }
 
         parent::tearDown();
     }
